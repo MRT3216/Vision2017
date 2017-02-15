d2080950b2d0dec6b752817dee62b988b69fc997
Tkdefender88 <tkdefender88@gmail.com>
2017-02-14 23:31:23 -0700

    Some more tweaks and changes added to LT
    Specifically in the filiter countour method

--- LiftTracker/src/LiftTracker.java
+++ LiftTracker/src/LiftTracker.java
@@ -16,6 +16,7 @@ public class LiftTracker {
 		private Mat hsvThresholdOutput = new Mat();
 		private Mat cvErodeOutput = new Mat();
 		private ArrayList<MatOfPoint> findContoursOutput = new ArrayList<MatOfPoint>();
+		ArrayList<MatOfPoint> filterContoursOutput = new ArrayList<MatOfPoint>();
 
 		static {
 			System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
@@ -27,16 +28,16 @@ public class LiftTracker {
 		public void process(Mat source0) {
 			// Step HSV_Threshold0:
 			Mat hsvThresholdInput = source0;
-			double[] hsvThresholdHue = {33.99280575539568, 157.27272727272725};
-			double[] hsvThresholdSaturation = {0.0, 83.28282828282829};
-			double[] hsvThresholdValue = {229.31654676258992, 255.0};
+			double[] hsvThresholdHue = {45.32374100719424, 77.69902413970206};
+			double[] hsvThresholdSaturation = {0.0, 41.36363636363637};
+			double[] hsvThresholdValue = {245.3954802259887, 255.0};
 			hsvThreshold(hsvThresholdInput, hsvThresholdHue, hsvThresholdSaturation, hsvThresholdValue, hsvThresholdOutput);
 
 			// Step CV_erode0:
 			Mat cvErodeSrc = hsvThresholdOutput;
 			Mat cvErodeKernel = new Mat();
 			Point cvErodeAnchor = new Point(-1, -1);
-			double cvErodeIterations = 1.0;
+			double cvErodeIterations = 2.0;
 			int cvErodeBordertype = Core.BORDER_CONSTANT;
 			Scalar cvErodeBordervalue = new Scalar(-1);
 			cvErode(cvErodeSrc, cvErodeKernel, cvErodeAnchor, cvErodeIterations, cvErodeBordertype, cvErodeBordervalue, cvErodeOutput);
@@ -45,6 +46,21 @@ public class LiftTracker {
 			Mat findContoursInput = cvErodeOutput;
 			boolean findContoursExternalOnly = false;
 			findContours(findContoursInput, findContoursExternalOnly, findContoursOutput);
+
+			// Step Filter_Contours0:
+			ArrayList<MatOfPoint> filterContoursContours = findContoursOutput;
+			double filterContoursMinArea = 125.0;
+			double filterContoursMinPerimeter = 0.0;
+			double filterContoursMinWidth = 0.0;
+			double filterContoursMaxWidth = 70.0;
+			double filterContoursMinHeight = 30.0;
+			double filterContoursMaxHeight = 600.0;
+			double[] filterContoursSolidity = {0.0, 100.0};
+			double filterContoursMaxVertices = 1000000.0;
+			double filterContoursMinVertices = 0.0;
+			double filterContoursMinRatio = 0.0;
+			double filterContoursMaxRatio = 1000.0;
+			filterContours(filterContoursContours, filterContoursMinArea, filterContoursMinPerimeter, filterContoursMinWidth, filterContoursMaxWidth, filterContoursMinHeight, filterContoursMaxHeight, filterContoursSolidity, filterContoursMaxVertices, filterContoursMinVertices, filterContoursMinRatio, filterContoursMaxRatio, filterContoursOutput);
 
 		}
 
@@ -70,6 +86,14 @@ public class LiftTracker {
 		 */
 		public ArrayList<MatOfPoint> findContoursOutput() {
 			return findContoursOutput;
+		}
+
+		/**
+		 * This method is a generated getter for the output of a Filter_Contours.
+		 * @return ArrayList<MatOfPoint> output from Filter_Contours.
+		 */
+		public ArrayList<MatOfPoint> filterContoursOutput() {
+			return filterContoursOutput;
 		}
 
 
@@ -133,6 +157,55 @@ public class LiftTracker {
 			}
 			int method = Imgproc.CHAIN_APPROX_SIMPLE;
 			Imgproc.findContours(input, contours, hierarchy, mode, method);
+		}
+
+
+		/**
+		 * Filters out contours that do not meet certain criteria.
+		 * @param inputContours is the input list of contours
+		 * @param output is the the output list of contours
+		 * @param minArea is the minimum area of a contour that will be kept
+		 * @param minPerimeter is the minimum perimeter of a contour that will be kept
+		 * @param minWidth minimum width of a contour
+		 * @param maxWidth maximum width
+		 * @param minHeight minimum height
+		 * @param maxHeight maximimum height
+		 * @param Solidity the minimum and maximum solidity of a contour
+		 * @param minVertexCount minimum vertex Count of the contours
+		 * @param maxVertexCount maximum vertex Count
+		 * @param minRatio minimum ratio of width to height
+		 * @param maxRatio maximum ratio of width to height
+		 */
+		private void filterContours(List<MatOfPoint> inputContours, double minArea,
+			double minPerimeter, double minWidth, double maxWidth, double minHeight, double
+			maxHeight, double[] solidity, double maxVertexCount, double minVertexCount, double
+			minRatio, double maxRatio, List<MatOfPoint> output) {
+			final MatOfInt hull = new MatOfInt();
+			output.clear();
+			//operation
+			for (int i = 0; i < inputContours.size(); i++) {
+				final MatOfPoint contour = inputContours.get(i);
+				final Rect bb = Imgproc.boundingRect(contour);
+				if (bb.width < minWidth || bb.width > maxWidth) continue;
+				if (bb.height < minHeight || bb.height > maxHeight) continue;
+				final double area = Imgproc.contourArea(contour);
+				if (area < minArea) continue;
+				if (Imgproc.arcLength(new MatOfPoint2f(contour.toArray()), true) < minPerimeter) continue;
+				Imgproc.convexHull(contour, hull);
+				MatOfPoint mopHull = new MatOfPoint();
+				mopHull.create((int) hull.size().height, 1, CvType.CV_32SC2);
+				for (int j = 0; j < hull.size().height; j++) {
+					int index = (int)hull.get(j, 0)[0];
+					double[] point = new double[] { contour.get(index, 0)[0], contour.get(index, 0)[1]};
+					mopHull.put(j, 0, point);
+				}
+				final double solid = 100 * area / Imgproc.contourArea(mopHull);
+				if (solid < solidity[0] || solid > solidity[1]) continue;
+				if (contour.rows() < minVertexCount || contour.rows() > maxVertexCount)	continue;
+				final double ratio = bb.width / (double)bb.height;
+				if (ratio < minRatio || ratio > maxRatio) continue;
+				output.add(contour);
+			}
 		}
 
 }
